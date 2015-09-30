9be714a2db61fa9dc0e6ec04dedc9f7a83a26364
usuario <usuario@10.22.143.15>
2015-09-30 10:02:39 +0100

    Práctica 2 de IS2 de creación de un Histograma con HashMap

--- src/práctica/pkg2/is2/Práctica2IS2.java
+++ src/práctica/pkg2/is2/Práctica2IS2.java
@@ -6,6 +6,8 @@ public class Práctica2IS2 {
         Integer [] array = {1,1,1,4,4,2,2,2,100};
         String[] array1 = {"Ana","Juan","Pedro","Ana","Juan"};
         Histogram myHistogram = new Histogram(array);
+        Histogram myHistogram1 = new Histogram(array);
         System.out.println(myHistogram.getHistogram());
+        System.out.println(myHistogram1.getHistogram());
     }
 }
